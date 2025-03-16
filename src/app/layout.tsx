d818b0224baf0e3f3f24a98aa54799b5447e6aff
natoon16 <rafaelbagay48@gmail.com>
2025-03-16 13:08:41 -0400

    Add Bing Webmaster Tools verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: {
     default: 'Florida Wedding Directory',
     template: '%s | Florida Wedding Directory',
@@ -20,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <meta name="msvalidate.01" content="82BB97B4BF4623390ACE010D6D6948BE" />
+      </head>
       <body className={`${inter.className} min-h-full flex flex-col`}>
         <Header />
         <main className="flex-grow relative">

8f7d72a7ea0a64e1d7a32adb8f3110c00f0be99f
natoon16 <rafaelbagay48@gmail.com>
2025-03-21 10:49:39 -0400

    feat: Add category and city pages with Google Places integration

---
+++ app/data/keywords.ts
@@ -0,0 +1,63 @@
+export interface Category {
+  name: string;
+  title: string;
+  slug: string;
+}
+
+export const categories: Category[] = [
+  {
+    name: 'venue',
+    title: 'Wedding Venues',
+    slug: 'venues'
+  },
+  {
+    name: 'photographer',
+    title: 'Wedding Photographers',
+    slug: 'photographers'
+  },
+  {
+    name: 'videographer',
+    title: 'Wedding Videographers',
+    slug: 'videographers'
+  },
+  {
+    name: 'dj',
+    title: 'Wedding DJs',
+    slug: 'djs'
+  },
+  {
+    name: 'florist',
+    title: 'Wedding Florists',
+    slug: 'florists'
+  },
+  {
+    name: 'caterer',
+    title: 'Wedding Caterers',
+    slug: 'caterers'
+  },
+  {
+    name: 'planner',
+    title: 'Wedding Planners',
+    slug: 'planners'
+  },
+  {
+    name: 'bakery',
+    title: 'Wedding Cake Bakeries',
+    slug: 'bakeries'
+  },
+  {
+    name: 'dress',
+    title: 'Wedding Dress Shops',
+    slug: 'dresses'
+  },
+  {
+    name: 'makeup artist',
+    title: 'Wedding Makeup Artists',
+    slug: 'makeup-artists'
+  },
+  {
+    name: 'hair stylist',
+    title: 'Wedding Hair Stylists',
+    slug: 'hair-stylists'
+  }
+]; 

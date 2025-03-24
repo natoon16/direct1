9e989830f3dd958f144e3aeb4141c7e21e8212cc
natoon16 <rafaelbagay48@gmail.com>
2025-03-24 13:22:41 -0400

    Fix next-intl configuration by moving it to the correct location

--- i18n/request.ts
+++ i18n/request.ts
@@ -10,6 +10,6 @@ export default getRequestConfig(async ({ locale }) => {
 
   return {
     locale: locale as Locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 }); 
